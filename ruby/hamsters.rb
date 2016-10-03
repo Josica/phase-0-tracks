0e074bfd35343d4ceb3c64a5b21eaaf6462692e9
Josica <josica.ganesamurthy@gmail.com>
2016-10-03 16:52:55 -0400

    Release 2

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,14 +3,23 @@ puts "What is your hamster's name?"
 name = gets.chomp
 puts "What is your hamster's volume level from 1 - 10?"
 volume = gets.chomp
+volume = volume.to_i
 puts "What is your hamster's fur color?"
 fur = gets.chomp
-puts "Is your hamster a good candidate for adoption?"
+puts "Is your hamster a good candidate for adoption? (\"Y\" for Yes| \"N\" for No)"
 adopt = gets.chomp
-puts "Do you know the age of your hamster? (Y for Yes|  for No)"
+if adopt == "N"
+	adopt = false
+elsif adopt == "Y"
+	adopt = true 
+end
+puts "What is the age of your hamster?"
 age = gets.chomp 
 if age == "" 
 	age = nil 
+elsif age !=""
+	age = age.to_i
 end 
+
 
 
