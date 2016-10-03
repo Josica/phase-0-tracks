3ffe20aa7e9b1f5dfe7e02d1275206a88f94acea
Josica <josica.ganesamurthy@gmail.com>
2016-10-03 16:40:55 -0400

    Initial commit - Release 1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,16 @@
+puts "Welcome to Our Hamster Sanctuary"
+puts "What is your hamster's name?"
+name = gets.chomp
+puts "What is your hamster's volume level from 1 - 10?"
+volume = gets.chomp
+puts "What is your hamster's fur color?"
+fur = gets.chomp
+puts "Is your hamster a good candidate for adoption?"
+adopt = gets.chomp
+puts "Do you know the age of your hamster? (Y for Yes|  for No)"
+age = gets.chomp 
+if age == "" 
+	age = nil 
+end 
+
+
