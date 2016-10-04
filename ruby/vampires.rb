972988e14498faebb83fd356fed641d3a4cbbcb3
Josica <josica.ganesamurthy@gmail.com>
2016-10-04 12:46:07 -0400

    Release 1: Interview with the Vampire

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "Welcome to the Werewolf Inc Interview"
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
