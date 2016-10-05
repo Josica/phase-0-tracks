6433f02a189413a77f49cc28fd0b43da8851037b
Josica <josica.ganesamurthy@gmail.com>
2016-10-04 22:43:03 -0400

    Release 2: Detection Logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,9 +3,43 @@ puts "What is your name?"
 name = gets.chomp
 puts "How old are you?"
 age = gets.chomp
+age = age.to_i
 puts "What year were you born?"
 year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+year = year.to_i
+if age == (2016 - year)
+	correct_age = true
+else 
+	correct_age = false 
+end 
+puts "Our company cafeteria serves garlic bread."
+puts "Should we order some for you? (\"Y\" for Yes| \"N\" for No)"
 garlic_bread = gets.chomp
+if garlic_bread == "N"
+	garlic_bread = false
+elsif garlic_bread == "Y"
+	garlic_bread = true 
+end
 puts "Would you like to enroll in the company’s health insurance?"
+puts "(\"Y\" for Yes| \"N\" for No)"
 health_insurance = gets.chomp
+if health_insurance == "N"
+	health_insurance = false
+elsif health_insurance == "Y"
+	health_insurance = true 
+end
+
+puts "*************************** Vampire Detection Results ******************************"
+
+if name == "Drake Cula" || name == "Tu Fang"
+	puts "Definitely a vampire."
+elsif correct_age == true && (garlic_bread == true || health_insurance == true)
+	puts "Probably not a vampire."
+elsif correct_age == false && (garlic_bread == false || health_insurance == false)
+	puts "Probably a vampire."
+elsif (correct_age == false || garlic_bread == false) && health_insurance == false
+	puts "Almost certainly a vampire."
+else 
+	puts "Results inconclusive."
+end 
+puts "************************************************************************************"
