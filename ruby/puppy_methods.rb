345a7b3f84adfd5fd58eb533c38f85a98f0d1e62
Josica <josica.ganesamurthy@gmail.com>
2016-10-18 13:33:22 -0400

    Add Dancer class and class methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,42 +1,70 @@
-class Puppy
-	
-   def initialize
-  	puts "Initializing new puppy instance..."
-  end
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#    def initialize
+#   	puts "Initializing new puppy instance..."
+#   end
 
-  def speak(number_of_barks)
-  	i = 0
-  	while i < number_of_barks 
-  		puts "woof"
-  		i += 1
-  	end
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-  def roll_over
-  	puts "roll over"
-  end
+#   def speak(number_of_barks)
+#   	i = 0
+#   	while i < number_of_barks 
+#   		puts "woof"
+#   		i += 1
+#   	end
+#   end
 
-  def dog_years(human_years)
-  	dog_age = human_years *  7
-  	puts dog_age
-  	return dog_age
-  end
+#   def roll_over
+#   	puts "roll over"
+#   end
 
-  def play_dead
-  	puts "play dead"
-  end
+#   def dog_years(human_years)
+#   	dog_age = human_years *  7
+#   	puts dog_age
+#   	return dog_age
+#   end
 
+#   def play_dead
+#   	puts "play dead"
+#   end
+
+# end
+
+# #Driver Code
+# kody = Puppy.new
+# kody.fetch("ball")
+# kody.speak(5)
+# kody.roll_over
+# kody.dog_years(6)
+# kody.play_dead
+
+class Dancer 
+
+def initialize 
+	puts "Initializing new dancer instance.."
+end 
+
+def jump 
+	puts "Jumping..."
 end
 
-#Driver Code
-kody = Puppy.new
-kody.fetch("ball")
-kody.speak(5)
-kody.roll_over
-kody.dog_years(6)
-kody.play_dead
+def twirl 
+	puts "Twirling..."
+end
+
+end 
+
+dance_team = []
+until dance_team.length >= 50
+	dancer = Dancer.new
+	dance_team.push dancer
+end 	
+
+dance_team.each do |x| 
+	x.jump 
+	x.twirl
+end 
+
