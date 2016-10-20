e8f0075b3805d6d0c41fcaed201e0992f55ea1aa
Josica <josica.ganesamurthy@gmail.com>
2016-10-19 22:39:34 -0400

    Test driver code for Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa 
 
-	def initialize(gender,ethnicity)
+	def initialize(gender, ethnicity)
 		@gender = gender 
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", 
@@ -17,14 +17,47 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end 
 
+	def celebrate_birthday
+		@age = @age +=1 
+	end
+
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete_if {|name| name == reindeer}
+		@reindeer_ranking << reindeer
+	end 
+
+	def age 
+		@age 
+	end 
+
+	def ethnicity
+		@ethnicity
+	end 
+
+	def gender
+		@gender
+	end 	
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end 
 end
 
-santas = []
-santas << Santa.new("Female", "Moroccan")
-santas << Santa.new("Transfeminine", "Guyanese")
-santas << Santa.new("Male", "Tamil")
-santas << Santa.new("Transsexual Male", "Polynesian")
-santas << Santa.new("Androgynous", "Sri Lankan")
-santas << Santa.new("Gender fluid", "Croatian")
-santas << Santa.new("Bigender", "Zimbabwean")
+# santas = []
+# santas << Santa.new("Female", "Moroccan")
+# santas << Santa.new("Transfeminine", "Guyanese")
+# santas << Santa.new("Male", "Tamil")
+# santas << Santa.new("Transsexual Male", "Polynesian")
+# santas << Santa.new("Androgynous", "Sri Lankan")
+# santas << Santa.new("Gender fluid", "Croatian")
+# santas << Santa.new("Bigender", "Zimbabwean")
+
+the_santa = Santa.new("Female", "Moroccan")
+puts "The reindeer is #{the_santa.ethnicity} and is #{the_santa.gender}."
+the_santa.gender = "Male"
+puts "Changed gender to #{the_santa.gender}"
+
+
+
+
 
