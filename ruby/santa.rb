8dbf7b9d065a8d3414a02e0decbbed629c7393f4
Josica <josica.ganesamurthy@gmail.com>
2016-10-19 21:33:34 -0400

    Release 1: Give Santa Attributes for Christmas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,11 @@
 class Santa 
 
-	def initialize 
+	def initialize(gender,ethnicity)
+		@gender = gender 
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", 
+			"Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 		puts "Initializing Santa instance ..."
 	end
 
@@ -14,6 +19,12 @@ class Santa
 
 end
 
-# Santa1 = Santa.new 
-# Santa1.speak 
+santas = []
+santas << Santa.new("Female", "Moroccan")
+santas << Santa.new("Transfeminine", "Guyanese")
+santas << Santa.new("Male", "Tamil")
+santas << Santa.new("Transsexual Male", "Polynesian")
+santas << Santa.new("Androgynous", "Sri Lankan")
+santas << Santa.new("Gender fluid", "Croatian")
+santas << Santa.new("Bigender", "Zimbabwean")
 
