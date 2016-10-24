cc39027f4223ef35bc34216d5ca5f1a781aee862
Josica <josica.ganesamurthy@gmail.com>
2016-10-24 11:57:20 -0400

    Remove max test

--- ruby/game/guesstheword_spec.rb
+++ ruby/game/guesstheword_spec.rb
@@ -7,9 +7,4 @@ describe GuessTheWordGame do
 		expect(game.initial_board) .to eq ("_ _ _ _ _ _ _ _ _ ")
 	end
 
-	it "will return nine spaces " do 
-		game = GuessTheWordGame.new("superhero","spaghetti")
-		expect(game.max) .to eq (9)
-	end
-
 end 
