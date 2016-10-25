cf092acf82218894f089a10c26e3a50f6733c749
Josica <josica.ganesamurthy@gmail.com>
2016-10-25 14:30:02 -0400

    Implement reverse string function

--- js/explore.js
+++ js/explore.js
@@ -6,3 +6,14 @@
 // Create a loop to put each letter in the given 
 // string in reversed order in the output string 
 // and then return the output string that is reversed. 
+
+function reverse_string(str) {
+var reversed_str = "";
+
+for (var i = 0; i < str.length; i++) {
+	reversed_str = str[i] + reversed_str;
+}
+
+return reversed_str;
+
+}
