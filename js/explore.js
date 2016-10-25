c1666714d0f00d38c5d82dc5f939b40d502fdc4a
Josica <josica.ganesamurthy@gmail.com>
2016-10-25 14:26:40 -0400

    Add psuedocode

---
+++ js/explore.js
@@ -0,0 +1,8 @@
+// Psuedocode
+
+// Create a function that that takes a 
+// string as a parameter and reverses the string. 
+// Set a variable for the output (the reversed string)
+// Create a loop to put each letter in the given 
+// string in reversed order in the output string 
+// and then return the output string that is reversed. 
