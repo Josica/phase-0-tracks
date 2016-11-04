7ebb3ddcf4f06ce93fe518ecb664d0812e80d706
Josica <josica.ganesamurthy@gmail.com>
2016-11-03 20:13:18 -0400

    Create table for spots

--- databases/studyspot/studyspots.rb
+++ databases/studyspot/studyspots.rb
@@ -3,3 +3,17 @@ require 'sqlite3'
 
 #create database
 db = SQLite3::Database.new("spots.db")
+
+#create table for spots (if it's not there already)
+create_table = <<-SQL 
+CREATE TABLE IF NOT EXISTS spots (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  borough VARCHAR(255),
+  phone VARCHAR(255),
+  address VARCHAR(255),
+  accessibility VARCHAR(255)
+SQL 
+
+db.execute(create_table)
