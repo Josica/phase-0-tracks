afef2c60bfc2427b20811ee6c4ccfc354fe9aad9
Josica <josica.ganesamurthy@gmail.com>
2016-11-09 14:37:51 -0500

    Add an event listener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,4 +1,9 @@
 
-var photo = document.getElementById("elephant");
-photo.style.visibility = "hidden";
+function hideElephant(){
+var elephant = document.getElementById("elephant");
+elephant.style.visibility = "hidden";
+}
+var button = document.getElementById("click");
+button.addEventListener("click", hideElephant);
+
 
